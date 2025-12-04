f99a19e553a1a7842bd8205bcd6c69e7ca3c4dc6
Megha-P-53 <1053meghaprakash@gmail.com>
2025-12-04 09:10:05 +0530

    Added backend as normal folder (fixed submodule)

---
+++ employeeapp/src/main/java/in/MeghaP/model/Employee.java
@@ -0,0 +1,22 @@
+package in.MeghaP.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String phone;
+    private String department;
+    private Double salary;
+}
